d5071f6ed735f0ab93e919105c36dac2f9a8a62b
Martin Juhasz <info@martinjuhasz.de>
2020-01-13 19:53:07 +0100

    chore(gatsby): added gatsby configs

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	siteTitle: 'Martin Juhasz - Software Crafter', // <title>
+	manifestName: 'Martin Juhasz',
+	manifestShortName: 'juhasz.io', // max 12 characters
+	manifestStartUrl: '/',
+	manifestBackgroundColor: '#304f96',
+	manifestThemeColor: '#304f96',
+	manifestDisplay: 'standalone',
+	manifestIcon: 'src/assets/images/website-icon.png',
+	heading: 'juhasz.io',
+	subHeading:
+		'Software Crafter building useful and solid applications for mobile and the web.',
+	keywords:
+		'sofware, developer, mobile developer, web developer, crafter, computer scientist, freelance',
+	// social
+	socialLinks: [
+		{
+			icon: 'fa-envelope',
+			name: 'martin@juhasz.io',
+			url: 'mailto:martin@juhasz.io',
+		},
+		{
+			icon: 'fa-twitter',
+			name: 'Twitter',
+			url: 'https://twitter.com/martinjuhasz',
+		},
+	],
+	phone: '+49 171 6464863',
+	address: 'Nibelungenstr. 28, 64560 Riedstadt',
+}
